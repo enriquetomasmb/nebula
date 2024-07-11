2dcf37842102c6c07699b7293858be560967346f
Miguelfernandez3 <miguelfernandezllamas@gmail.com>
2024-07-12 01:34:38 +0200

    Add functions for calculating knowledge distillation loss

---
+++ nebula/core/knoledgeDistillation/KD.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DistillKL(nn.Module):
+    """Distilling the Knowledge in a Neural Network"""
+    def __init__(self, T):
+        super(DistillKL, self).__init__()
+        self.T = T
+
+    def forward(self, y_s, y_t):
+        p_s = F.log_softmax(y_s/self.T, dim=1)
+        p_t = F.softmax(y_t/self.T, dim=1)
+        loss = nn.KLDivLoss(reduction='batchmean')(p_s, p_t) * (self.T**2)
+        return loss
