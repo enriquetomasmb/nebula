815332fd4fcf5f2c4ea746cecc69e17fdcdb4d9b
Miguelfernandez3 <miguelfernandezllamas@gmail.com>
2024-07-16 17:25:26 +0200

    Added models for different implementation in MNIST dataset

---
+++ nebula/core/models/mnist/prototypeKnoledgeDistillation/ProtoStudentCNN.py
@@ -0,0 +1,192 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from nebula.core.knoledgeDistillation.KD import DistillKL
+from nebula.core.models.mnist.prototypeKnoledgeDistillation.ProtoTeacherCNN import \
+    MDProtoTeacherMNISTModelCNN, ProtoTeacherMNISTModelCNN
+from nebula.core.models.prototypeKnoledgeDistillationBaseNebulaModel.protostudentfedstellarmodel import ProtoStudentFedstellarModel
+
+class ProtoStudentMNISTModelCNN(ProtoStudentFedstellarModel):
+    """
+    LightningModule for MNIST.
+    """
+
+
+    def __init__(
+            self,
+            input_channels=1,
+            num_classes=10,
+            learning_rate=1e-3,
+            metrics=None,
+            confusion_matrix=None,
+            seed=None,
+            teacher_model=None,
+            T=2,
+            beta_kd=1,
+            beta_proto=1,
+            mutual_distilation=False,
+            teacher_beta=100,
+            send_logic=None
+    ):
+        if teacher_model is None:
+            if mutual_distilation:
+                teacher_model = MDProtoTeacherMNISTModelCNN(beta=teacher_beta)
+            else:
+                teacher_model = ProtoTeacherMNISTModelCNN()
+
+        super().__init__(input_channels, num_classes, learning_rate, metrics, confusion_matrix, seed, teacher_model, T,mutual_distilation,send_logic)
+
+
+        self.example_input_array = torch.zeros(1, 1, 28, 28)
+        self.beta_proto = beta_proto
+        self.beta_kd = beta_kd
+        self.criterion_nll = nn.NLLLoss()
+        self.criterion_mse = torch.nn.MSELoss()
+        self.criterion_cls = torch.nn.CrossEntropyLoss()
+        self.criterion_div = DistillKL(self.T)
+
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=input_channels, out_channels=32, kernel_size=(5, 5), padding="same"
+        )
+        self.relu = torch.nn.ReLU()
+        self.pool1 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv2 = torch.nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=(5, 5), padding="same"
+        )
+        self.pool2 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.l1 = torch.nn.Linear(7 * 7 * 64, 2048)
+        self.l2 = torch.nn.Linear(2048, num_classes)
+
+
+
+    def forward_train(self, x, is_feat=False, softmax=True):
+        """Forward pass only for train the model.
+            is_feat: bool, if True return the features of the model.
+            softmax: bool, if True apply softmax to the logits.
+        """
+        # Reshape the input tensor
+        input_layer = x.view(-1, 1, 28, 28)
+
+        # First convolutional layer
+        conv1 = self.relu(self.conv1(input_layer))
+        pool1 = self.pool1(conv1)
+
+        # Second convolutional layer
+        conv2 = self.relu(self.conv2(pool1))
+        pool2 = self.pool2(conv2)
+
+        # Flatten the tensor
+        pool2_flat = pool2.reshape(-1, 7 * 7 * 64)
+
+        # Fully connected layers
+        dense = self.relu(self.l1(pool2_flat))
+        logits = self.l2(dense)
+
+        if is_feat:
+            if softmax:
+                return F.log_softmax(logits, dim=1), dense, [conv1, conv2]
+            else:
+                return logits, dense, [conv1, conv2]
+        else:
+            if softmax:
+                return F.log_softmax(logits, dim=1), dense
+            else:
+                return logits, dense
+
+
+    def forward(self, x):
+        """Forward pass for inference the model, if model have prototypes"""
+        if len(self.global_protos) == 0:
+            logits, _ = self.forward_train(x)
+            return logits
+
+        # Reshape the input tensor
+        input_layer = x.view(-1, 1, 28, 28)
+
+        # First convolutional layer
+        conv1 = self.relu(self.conv1(input_layer))
+        pool1 = self.pool1(conv1)
+
+        # Second convolutional layer
+        conv2 = self.relu(self.conv2(pool1))
+        pool2 = self.pool2(conv2)
+
+        # Flatten the tensor
+        pool2_flat = pool2.reshape(-1, 7 * 7 * 64)
+
+        # Fully connected layers
+        dense = self.relu(self.l1(pool2_flat))
+
+        # Calculate distances
+        distances = []
+        for key, proto in self.global_protos.items():
+            # Calculate Euclidean distance
+            proto = proto.to(dense.device)
+            dist = torch.norm(dense - proto, dim=1)
+            distances.append(dist.unsqueeze(1))
+        distances = torch.cat(distances, dim=1)
+
+        # Return the predicted class based on the closest prototype
+        return distances.argmin(dim=1)
+
+    def configure_optimizers(self):
+        """ Configure the optimizer for training. """
+        optimizer = torch.optim.Adam(
+            self.parameters(),
+            lr=self.learning_rate,
+            betas=(self.config["beta1"], self.config["beta2"]),
+            amsgrad=self.config["amsgrad"],
+        )
+        return optimizer
+
+    def step(self, batch, batch_idx, phase):
+
+        images, labels_g = batch
+        images, labels = images.to(self.device), labels_g.to(self.device)
+        logits, protos = self.forward_train(images, softmax=False)
+
+        logits_softmax = F.log_softmax(logits, dim=1)
+        protos_copy = protos.clone()
+        with torch.no_grad():
+            teacher_logits, teacher_protos = self.teacher_model.forward_train(images, softmax=False)
+
+        # Compute loss 1
+        loss1 = self.criterion_nll(logits_softmax, labels)
+
+        # Compute loss 2
+        if len(self.global_protos) == 0:
+            loss2 = 0 * loss1
+        else:
+            proto_new = protos.clone()
+            i = 0
+            for label in labels:
+                if label.item() in self.global_protos.keys():
+                    proto_new[i, :] = self.global_protos[label.item()].data
+                i += 1
+            # Compute the loss for the prototypes
+            loss2 = self.criterion_mse(proto_new, protos)
+
+        # Compute loss knowledge distillation
+        loss3 = self.criterion_div(logits, teacher_logits)
+
+        # Compute loss knowledge distillation for the prototypes
+        loss4 = self.criterion_mse(protos_copy, teacher_protos)
+
+        # Combine the losses
+        loss = loss1 + self.beta_proto*loss2 + self.beta_kd*(0.5*loss3 + 0.5*loss4)
+
+        self.process_metrics(phase, logits, labels, loss)
+
+        if phase == "Train":
+            # Update the prototypes
+            self.model_updated_flag2 = True
+            for i in range(len(labels_g)):
+                label = labels_g[i].item()
+                if label not in self.agg_protos_label:
+                    self.agg_protos_label[label] = dict(sum=torch.zeros_like(protos[i, :]), count=0)
+                self.agg_protos_label[label]['sum'] += protos[i, :].detach().clone()
+                self.agg_protos_label[label]['count'] += 1
+
+        return loss
+
---
+++ nebula/core/models/mnist/prototypeKnoledgeDistillation/ProtoTeacherCNN.py
@@ -0,0 +1,350 @@
+import copy
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from nebula.core.knoledgeDistillation.AT import Attention
+
+import logging
+
+from nebula.core.models.prototypeKnoledgeDistillationBaseNebulaModel.prototeacherfedstellarmodel import ProtoTeacherFedstellarModel
+
+
+class ProtoTeacherMNISTModelCNN(ProtoTeacherFedstellarModel):
+    """
+    LightningModule for MNIST.
+    """
+
+
+    def __init__(
+            self,
+            input_channels=1,
+            num_classes=10,
+            learning_rate=1e-3,
+            metrics=None,
+            confusion_matrix=None,
+            seed=None,
+            beta=1
+    ):
+        super().__init__(input_channels, num_classes, learning_rate, metrics, confusion_matrix, seed)
+
+        self.example_input_array = torch.zeros(1, 1, 28, 28)
+        self.beta = beta
+        self.criterion_nll = nn.NLLLoss()
+        self.criterion_mse = torch.nn.MSELoss()
+
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=input_channels, out_channels=64, kernel_size=(5, 5), padding="same"
+        )
+        self.relu = torch.nn.ReLU()
+        self.pool1 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv2 = torch.nn.Conv2d(
+            in_channels=64, out_channels=128, kernel_size=(5, 5), padding="same"
+        )
+        self.pool2 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.l1 = torch.nn.Linear(7 * 7 * 128, 2048)
+        self.l2 = torch.nn.Linear(2048, num_classes)
+
+
+
+    def forward_train(self, x, is_feat=False, softmax=True):
+        """Forward pass only for train the model.
+            is_feat: bool, if True return the features of the model.
+            softmax: bool, if True apply softmax to the logits.
+        """
+        # Reshape the input tensor
+        input_layer = x.view(-1, 1, 28, 28)
+
+        # First convolutional layer
+        conv1 = self.relu(self.conv1(input_layer))
+        pool1 = self.pool1(conv1)
+
+        # Second convolutional layer
+        conv2 = self.relu(self.conv2(pool1))
+        pool2 = self.pool2(conv2)
+
+        # Flatten the tensor
+        pool2_flat = pool2.reshape(-1, 7 * 7 * 128)
+
+        # Fully connected layers
+        dense = self.relu(self.l1(pool2_flat))
+        logits = self.l2(dense)
+
+        if is_feat:
+            if softmax:
+                return F.log_softmax(logits, dim=1), dense, [conv1, conv2]
+            else:
+                return logits, dense, [conv1, conv2]
+        else:
+            if softmax:
+                return F.log_softmax(logits, dim=1), dense
+            else:
+                return logits, dense
+
+
+    def forward(self, x):
+        """Forward pass for inference the model, if model have prototypes"""
+        if len(self.global_protos) == 0:
+            logits, _ = self.forward_train(x)
+            return logits
+
+        # Reshape the input tensor
+        input_layer = x.view(-1, 1, 28, 28)
+
+        # First convolutional layer
+        conv1 = self.relu(self.conv1(input_layer))
+        pool1 = self.pool1(conv1)
+
+        # Second convolutional layer
+        conv2 = self.relu(self.conv2(pool1))
+        pool2 = self.pool2(conv2)
+
+        # Flatten the tensor
+        pool2_flat = pool2.reshape(-1, 7 * 7 * 128)
+
+        # Fully connected layers
+        dense = self.relu(self.l1(pool2_flat))
+
+        # Calculate distances
+        distances = []
+        for key, proto in self.global_protos.items():
+            # Calculate Euclidean distance
+            # send protos and dense to the same device
+            proto = proto.to(dense.device)
+            dist = torch.norm(dense - proto, dim=1)
+            distances.append(dist.unsqueeze(1))
+        distances = torch.cat(distances, dim=1)
+
+        # Return the predicted class based on the closest prototype
+        return distances.argmin(dim=1)
+
+    def configure_optimizers(self):
+        """ Configure the optimizer for training. """
+        optimizer = torch.optim.Adam(
+            self.parameters(),
+            lr=self.learning_rate,
+            betas=(self.config["beta1"], self.config["beta2"]),
+            amsgrad=self.config["amsgrad"],
+        )
+        return optimizer
+
+    def step(self, batch, batch_idx, phase):
+
+        images, labels_g = batch
+        images, labels = images.to(self.device), labels_g.to(self.device)
+        logits, protos = self.forward_train(images)
+
+        # Compute loss cross entropy loss
+        loss1 = self.criterion_nll(logits, labels)
+
+        # Compute loss 2
+        if len(self.global_protos) == 0:
+            loss2 = 0 * loss1
+        else:
+            proto_new = protos.clone()
+            i = 0
+            for label in labels:
+                if label.item() in self.global_protos.keys():
+                    proto_new[i, :] = self.global_protos[label.item()].data
+                i += 1
+            # Compute the loss for the prototypes
+            loss2 = self.criterion_mse(proto_new, protos)
+
+        # Combine the losses
+        loss = loss1 + self.beta*loss2
+        self.process_metrics(phase, logits, labels, loss)
+
+        if phase == "Train":
+            # Aggregate the prototypes
+            for i in range(len(labels_g)):
+                label = labels_g[i].item()
+                if label not in self.agg_protos_label:
+                    self.agg_protos_label[label] = dict(sum=torch.zeros_like(protos[i, :]), count=0)
+                self.agg_protos_label[label]['sum'] += protos[i, :].detach().clone()
+                self.agg_protos_label[label]['count'] += 1
+
+        return loss
+
+
+
+class MDProtoTeacherMNISTModelCNN(ProtoTeacherFedstellarModel):
+    """
+    LightningModule for MNIST.
+    """
+
+
+    def __init__(
+            self,
+            input_channels=1,
+            num_classes=10,
+            learning_rate=1e-3,
+            metrics=None,
+            confusion_matrix=None,
+            seed=None,
+            beta=1,
+            p=2,
+            beta_md=1000
+    ):
+        super().__init__(input_channels, num_classes, learning_rate, metrics, confusion_matrix, seed)
+        self.p = p
+        self.beta_md = beta_md
+        self.example_input_array = torch.zeros(1, 1, 28, 28)
+        self.learning_rate = learning_rate
+        self.beta = beta
+        self.criterion_nll = nn.NLLLoss()
+        self.criterion_mse = torch.nn.MSELoss()
+        self.criterion_kd = Attention(self.p)
+        self.student_model = None
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=input_channels, out_channels=64, kernel_size=(5, 5), padding="same"
+        )
+        self.relu = torch.nn.ReLU()
+        self.pool1 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv2 = torch.nn.Conv2d(
+            in_channels=64, out_channels=128, kernel_size=(5, 5), padding="same"
+        )
+        self.pool2 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.l1 = torch.nn.Linear(7 * 7 * 128, 2048)
+        self.l2 = torch.nn.Linear(2048, num_classes)
+
+
+
+    def forward_train(self, x, is_feat=False, softmax=True):
+        """Forward pass only for train the model.
+            is_feat: bool, if True return the features of the model.
+            softmax: bool, if True apply softmax to the logits.
+        """
+        # Reshape the input tensor
+        input_layer = x.view(-1, 1, 28, 28)
+
+        # First convolutional layer
+        conv1 = self.relu(self.conv1(input_layer))
+        pool1 = self.pool1(conv1)
+
+        # Second convolutional layer
+        conv2 = self.relu(self.conv2(pool1))
+        pool2 = self.pool2(conv2)
+
+        # Flatten the tensor
+        pool2_flat = pool2.reshape(-1, 7 * 7 * 128)
+
+        # Fully connected layers
+        dense = self.relu(self.l1(pool2_flat))
+        logits = self.l2(dense)
+
+        if is_feat:
+            if softmax:
+                return F.log_softmax(logits, dim=1), dense, [conv1, conv2]
+            else:
+                return logits, dense, [conv1, conv2]
+        else:
+            if softmax:
+                return F.log_softmax(logits, dim=1), dense
+            else:
+                return logits, dense
+
+
+    def forward(self, x):
+        """Forward pass for inference the model, if model have prototypes"""
+        if len(self.global_protos) == 0:
+            logits, _ = self.forward_train(x)
+            return logits
+
+        # Reshape the input tensor
+        input_layer = x.view(-1, 1, 28, 28)
+
+        # First convolutional layer
+        conv1 = self.relu(self.conv1(input_layer))
+        pool1 = self.pool1(conv1)
+
+        # Second convolutional layer
+        conv2 = self.relu(self.conv2(pool1))
+        pool2 = self.pool2(conv2)
+
+        # Flatten the tensor
+        pool2_flat = pool2.reshape(-1, 7 * 7 * 128)
+
+        # Fully connected layers
+        dense = self.relu(self.l1(pool2_flat))
+
+        # Calculate distances
+        distances = []
+        for key, proto in self.global_protos.items():
+            # Calculate Euclidean distance
+            proto = proto.to(dense.device)
+            dist = torch.norm(dense - proto, dim=1)
+            distances.append(dist.unsqueeze(1))
+        distances = torch.cat(distances, dim=1)
+
+        # Return the predicted class based on the closest prototype
+        return distances.argmin(dim=1)
+
+    def configure_optimizers(self):
+        """ Configure the optimizer for training. """
+        optimizer = torch.optim.Adam(
+            self.parameters(),
+            lr=self.learning_rate,
+            betas=(self.config["beta1"], self.config["beta2"]),
+            amsgrad=self.config["amsgrad"],
+        )
+        return optimizer
+
+    def step(self, batch, batch_idx, phase):
+
+        images, labels_g = batch
+        images, labels = images.to(self.device), labels_g.to(self.device)
+        logits, protos, feat_t = self.forward_train(images, is_feat=True)
+
+        # Compute loss cross entropy loss
+        loss_nll = self.criterion_nll(logits, labels)
+
+        # Compute loss 2
+        if len(self.global_protos) == 0:
+            loss_mse = 0 * loss_nll
+        else:
+            proto_new = protos.clone()
+            i = 0
+            for label in labels:
+                if label.item() in self.global_protos.keys():
+                    proto_new[i, :] = self.global_protos[label.item()].data
+                i += 1
+
+            # Compute the loss for the prototypes
+            loss_mse = self.criterion_mse(proto_new, protos)
+
+        if self.student_model is not None:
+            with torch.no_grad():
+                student_logits, student_protos, feat_s = self.student_model.forward_train(images, is_feat=True)
+                feat_s = [f.detach() for f in feat_s]
+            g_s = feat_s
+            g_t = feat_t
+            # Compute the mutual distillation loss
+            loss_group = self.criterion_kd(g_t, g_s)
+            loss_kd = sum(loss_group)
+        else:
+            loss_kd = 0 * loss_nll
+
+        # Combine the losses
+        loss = loss_nll + self.beta*loss_mse + self.beta_md*loss_kd
+        self.process_metrics(phase, logits, labels, loss)
+
+        if phase == "Train":
+            # Aggregate the prototypes
+            for i in range(len(labels_g)):
+                label = labels_g[i].item()
+                if label not in self.agg_protos_label:
+                    self.agg_protos_label[label] = dict(sum=torch.zeros_like(protos[i, :]), count=0)
+                self.agg_protos_label[label]['sum'] += protos[i, :].detach().clone()
+                self.agg_protos_label[label]['count'] += 1
+
+        return loss
+
+    def set_student_model(self, student_model):
+        """
+        For cyclic dependency problem, a copy of the student model is created, the teacher_model attribute is removed.
+        """
+        self.student_model = copy.deepcopy(student_model)
+        if hasattr(self.student_model, 'teacher_model'):
+            del self.student_model.teacher_model
+
