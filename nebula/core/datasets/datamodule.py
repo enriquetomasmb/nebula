35d3415d12843d22aeac9e5f136aed52ba9ffc89
enriquetomasmb <enriquetomasmb@gmail.com>
2024-10-17 20:55:09 +0200

    Reduce overhead and define pin memory

--- nebula/core/datasets/datamodule.py
+++ nebula/core/datasets/datamodule.py
@@ -124,7 +124,7 @@ class DataModule(LightningDataModule):
             shuffle=True,
             num_workers=self.num_workers,
             drop_last=True,
-            pin_memory=False,
+            pin_memory=True,
         )
 
     def val_dataloader(self):
@@ -137,7 +137,7 @@ class DataModule(LightningDataModule):
             shuffle=False,
             num_workers=self.num_workers,
             drop_last=True,
-            pin_memory=False,
+            pin_memory=True,
         )
 
     def test_dataloader(self):
@@ -152,7 +152,7 @@ class DataModule(LightningDataModule):
                 shuffle=False,
                 num_workers=self.num_workers,
                 drop_last=True,
-                pin_memory=False,
+                pin_memory=True,
             ),
             DataLoader(
                 self.global_te_subset,
@@ -160,7 +160,7 @@ class DataModule(LightningDataModule):
                 shuffle=False,
                 num_workers=self.num_workers,
                 drop_last=True,
-                pin_memory=False,
+                pin_memory=True,
             ),
         ]
 
@@ -178,5 +178,5 @@ class DataModule(LightningDataModule):
             sampler=random_sampler,
             num_workers=self.num_workers,
             drop_last=True,
-            pin_memory=False,
+            pin_memory=True,
         )
