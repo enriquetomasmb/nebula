70de2439f66116d6659d67556bf081c3d8bfe2e8
FerTV <fernando.torres.vega@gmail.com>
2024-11-05 15:58:26 +0100

    Refactor code in poisoning attacks to improve type checking and readability

--- nebula/addons/attacks/poisoning/labelflipping.py
+++ nebula/addons/attacks/poisoning/labelflipping.py
@@ -13,11 +13,33 @@ def labelFlipping(
     target_changed_label=7,
 ):
     """
-    select flipping_persent of labels, and change them to random values.
+    Flips the labels of a specified portion of a dataset to random values or to a specific target label.
+
+    This function modifies the labels of selected samples in the dataset based on the specified
+    poisoning percentage. Labels can be flipped either randomly or targeted to change from a specific
+    label to another specified label.
+
     Args:
-        dataset: the dataset of training data, torch.util.data.dataset like.
-        indices: Indices of subsets, list like.
-        flipping_persent: The ratio of labels want to change, float like.
+        dataset (Dataset): The dataset containing training data, expected to be a PyTorch dataset
+                           with a `.targets` attribute.
+        indices (list of int): The list of indices in the dataset to consider for label flipping.
+        poisoned_percent (float, optional): The ratio of labels to change, expressed as a fraction
+                                            (0 <= poisoned_percent <= 1). Default is 0.
+        targeted (bool, optional): If True, flips only labels matching `target_label` to `target_changed_label`.
+                                   Default is False.
+        target_label (int, optional): The label to change when `targeted` is True. Default is 4.
+        target_changed_label (int, optional): The label to which `target_label` will be changed. Default is 7.
+
+    Returns:
+        Dataset: A deep copy of the original dataset with modified labels in `.targets`.
+
+    Raises:
+        ValueError: If `poisoned_percent` is not between 0 and 1, or if `flipping_percent` is invalid.
+
+    Notes:
+        - When not in targeted mode, labels are flipped for a random selection of indices based on the specified
+          `poisoned_percent`. The new label is chosen randomly from the existing classes.
+        - In targeted mode, labels that match `target_label` are directly changed to `target_changed_label`.
     """
     new_dataset = copy.deepcopy(dataset)
     targets = new_dataset.targets.detach().clone()
@@ -26,7 +48,7 @@ def labelFlipping(
     # class_to_idx = new_dataset.class_to_idx
     # class_list = [class_to_idx[i] for i in classes]
     class_list = set(targets.tolist())
-    if targeted == False:
+    if not targeted:
         num_flipped = int(poisoned_persent * num_indices)
         if num_indices == 0:
             return new_dataset
