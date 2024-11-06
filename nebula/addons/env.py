6ea274c529e1e00cca0f5d57206d8b98b578f056
FerTV <fernando.torres.vega@gmail.com>
2024-11-06 15:45:36 +0100

    Refactor: Refactor function in topologymanager and add code descriptions - Refactored a function in topologymanager to improve efficiency and readability. - Added descriptions to the code in env.py, functionts.py, mobility.py, reporter.py, and topologymanager.py for better understanding of functionality.

--- nebula/addons/env.py
+++ nebula/addons/env.py
@@ -10,10 +10,29 @@ from nebula import __version__
 
 
 def check_version():
-    # Check version of NEBULA (__version__ is defined in __init__.py) and compare with __version__ in https://raw.githubusercontent.com/CyberDataLab/nebula/main/nebula/__init__.py
+    """
+    Checks the current version of NEBULA and compares it with the latest version available in the repository.
+
+    This function retrieves the latest NEBULA version from the specified GitHub repository and compares
+    it with the version defined in the local NEBULA package. If the versions do not match, it logs a message
+    prompting the user to update to the latest version.
+
+    Returns:
+        None
+
+    Raises:
+        SystemExit: If the version check fails or an exception occurs during the request.
+
+    Notes:
+        - The version information is expected to be defined in the `__init__.py` file of the NEBULA package
+          using the `__version__` variable.
+        - If the latest version is not the same as the local version, the program will exit after logging
+          the necessary information.
+        - An exception during the request will be logged, and the program will also exit.
+    """
     logging.info("Checking NEBULA version...")
     try:
-        r = requests.get("https://raw.githubusercontent.com/CyberDataLab/nebula/main/nebula/__init__.py")
+        r = requests.get("https://raw.githubusercontent.com/CyberDataLab/nebula/main/nebula/__init__.py", timeout=5)
         if r.status_code == 200:
             version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', r.text, re.MULTILINE).group(1)
             if version != __version__:
@@ -26,12 +45,30 @@ def check_version():
                 sys.exit(0)
             else:
                 logging.info(f"Your NEBULA version is {__version__} and it is the latest version.")
-    except Exception as e:
-        logging.exception(f"Error while checking NEBULA version: {e}")
+    except Exception:
+        logging.exception("Error while checking NEBULA version")
         sys.exit(0)
 
 
 def check_environment():
+    """
+    Logs the current environment configuration for the NEBULA platform.
+
+    This function gathers and logs information about the operating system, hardware, Python version,
+    PyTorch version (if installed), CPU configuration, and GPU configuration (if applicable). It provides
+    insights into the system's capabilities and current usage statistics.
+
+    Returns:
+        None
+
+    Notes:
+        - The function logs the NEBULA platform version using the `__version__` variable.
+        - It checks the system's CPU load, available memory, and detailed GPU statistics using the `pynvml`
+          library if running on Windows or Linux.
+        - If any of the libraries required for gathering information (like `torch`, `psutil`, or `pynvml`)
+          are not installed, appropriate log messages will be generated indicating the absence of that information.
+        - If any unexpected error occurs during execution, it will be logged as an exception.
+    """
     logging.info(f"NEBULA Platform version: {__version__}")
     # check_version()
 
@@ -46,7 +83,7 @@ def check_environment():
         logging.info("PyTorch version: " + torch.__version__)
     except ImportError:
         logging.info("PyTorch is not installed properly")
-    except Exception:
+    except Exception:  # noqa: S110
         pass
 
     logging.info("======== CPU Configuration ========")
@@ -62,7 +99,7 @@ def check_environment():
         )
     except ImportError:
         logging.info("No CPU information available")
-    except Exception:
+    except Exception:  # noqa: S110
         pass
 
     if sys.platform == "win32" or sys.platform == "linux":
@@ -93,7 +130,7 @@ def check_environment():
                 logging.info(f"GPU{i} fan speed: {gpu_fan_speed}")
         except ImportError:
             logging.info("pynvml module not found, GPU information unavailable")
-        except Exception:
+        except Exception:  # noqa: S110
             pass
     else:
         logging.info("GPU information unavailable")
--- nebula/addons/mobility.py
+++ nebula/addons/mobility.py
@@ -13,6 +13,40 @@ if TYPE_CHECKING:
 
 class Mobility:
     def __init__(self, config, cm: "CommunicationsManager"):
+        """
+        Initializes the mobility module with specified configuration and communication manager.
+
+        This method sets up the mobility parameters required for the module, including grace time,
+        geographical change interval, mobility type, and other network conditions based on distance.
+        It also logs the initialized settings for the mobility system.
+
+        Args:
+            config (Config): Configuration object containing mobility parameters and settings.
+            cm (CommunicationsManager): An instance of the CommunicationsManager class used for handling
+                                         communication-related tasks within the mobility module.
+
+        Attributes:
+            grace_time (float): Time allocated for mobility processes to stabilize.
+            period (float): Interval at which geographic changes are made.
+            mobility (bool): Flag indicating whether mobility is enabled.
+            mobility_type (str): Type of mobility strategy to be used (e.g., random, nearest).
+            radius_federation (float): Radius for federation in meters.
+            scheme_mobility (str): Scheme to be used for managing mobility.
+            round_frequency (int): Number of rounds after which mobility changes are applied.
+            max_distance_with_direct_connections (float): Maximum distance for direct connections in meters.
+            max_movement_random_strategy (float): Maximum movement distance for the random strategy in meters.
+            max_movement_nearest_strategy (float): Maximum movement distance for the nearest strategy in meters.
+            max_initiate_approximation (float): Maximum distance for initiating approximation calculations.
+            network_conditions (dict): A dictionary containing network conditions (bandwidth and delay)
+                                       based on distance.
+            current_network_conditions (dict): A dictionary mapping addresses to their current network conditions.
+
+        Logs:
+            Mobility information upon initialization to provide insights into the current setup.
+
+        Raises:
+            KeyError: If the expected mobility configuration keys are not found in the provided config.
+        """
         logging.info("Starting mobility module...")
         self.config = config
         self.cm = cm
@@ -43,12 +77,59 @@ class Mobility:
 
     @property
     def round(self):
+        """
+        Gets the current round number from the Communications Manager.
+
+        This property retrieves the current round number that is being managed by the
+        CommunicationsManager instance associated with this module. It provides an
+        interface to access the ongoing round of the communication process without
+        directly exposing the underlying method in the CommunicationsManager.
+
+        Returns:
+            int: The current round number managed by the CommunicationsManager.
+        """
         return self.cm.get_round()
 
     async def start(self):
-        asyncio.create_task(self.run_mobility())
+        """
+        Initiates the mobility process by starting the associated task.
+
+        This method creates and schedules an asynchronous task to run the
+        `run_mobility` coroutine, which handles the mobility operations
+        for the module. It allows the mobility operations to run concurrently
+        without blocking the execution of other tasks.
+
+        Returns:
+            asyncio.Task: An asyncio Task object representing the scheduled
+                           `run_mobility` operation.
+        """
+        task = asyncio.create_task(self.run_mobility())
+        return task
 
     async def run_mobility(self):
+        """
+        Executes the mobility operations in a continuous loop.
+
+        This coroutine manages the mobility behavior of the module. It first
+        checks whether mobility is enabled. If mobility is not enabled, the
+        function returns immediately.
+
+        If mobility is enabled, the function will wait for the specified
+        grace time before entering an infinite loop where it performs the
+        following operations:
+
+        1. Changes the geographical location by calling the `change_geo_location` method.
+        2. Adjusts connections based on the current distance by calling
+           the `change_connections_based_on_distance` method.
+        3. Sleeps for a specified period (`self.period`) before repeating the operations.
+
+        This allows for periodic updates to the module's geographical location
+        and network connections as per the defined mobility strategy.
+
+        Raises:
+            Exception: May raise exceptions if `change_geo_location` or
+                        `change_connections_based_on_distance` encounters errors.
+        """
         if not self.mobility:
             return
         await asyncio.sleep(self.grace_time)
@@ -58,11 +139,31 @@ class Mobility:
             await asyncio.sleep(self.period)
 
     async def change_geo_location_random_strategy(self, latitude, longitude):
+        """
+        Changes the geographical location of the entity using a random strategy.
+
+        This coroutine modifies the current geographical location by randomly
+        selecting a new position within a specified radius around the given
+        latitude and longitude. The new location is determined using polar
+        coordinates, where a random distance (radius) and angle are calculated.
+
+        Args:
+            latitude (float): The current latitude of the entity.
+            longitude (float): The current longitude of the entity.
+
+        Raises:
+            Exception: May raise exceptions if the `set_geo_location` method encounters errors.
+
+        Notes:
+            - The maximum movement distance is determined by `self.max_movement_random_strategy`.
+            - The calculated radius is converted from meters to degrees based on an approximate
+              conversion factor (1 degree is approximately 111 kilometers).
+        """
         logging.info("📍  Changing geo location randomly")
         # radius_in_degrees = self.radius_federation / 111000
         max_radius_in_degrees = self.max_movement_random_strategy / 111000
-        radius = random.uniform(0, max_radius_in_degrees)
-        angle = random.uniform(0, 2 * math.pi)
+        radius = random.uniform(0, max_radius_in_degrees)  # noqa: S311
+        angle = random.uniform(0, 2 * math.pi)  # noqa: S311
         latitude += radius * math.cos(angle)
         longitude += radius * math.sin(angle)
         await self.set_geo_location(latitude, longitude)
@@ -70,6 +171,30 @@ class Mobility:
     async def change_geo_location_nearest_neighbor_strategy(
         self, distance, latitude, longitude, neighbor_latitude, neighbor_longitude
     ):
+        """
+        Changes the geographical location of the entity towards the nearest neighbor.
+
+        This coroutine updates the current geographical location by calculating the direction
+        and distance to the nearest neighbor's coordinates. The movement towards the neighbor
+        is scaled based on the distance and the maximum movement allowed.
+
+        Args:
+            distance (float): The distance to the nearest neighbor.
+            latitude (float): The current latitude of the entity.
+            longitude (float): The current longitude of the entity.
+            neighbor_latitude (float): The latitude of the nearest neighbor.
+            neighbor_longitude (float): The longitude of the nearest neighbor.
+
+        Raises:
+            Exception: May raise exceptions if the `set_geo_location` method encounters errors.
+
+        Notes:
+            - The movement is scaled based on the maximum allowed distance defined by
+              `self.max_movement_nearest_strategy`.
+            - The angle to the neighbor is calculated using the arctangent of the difference in
+              coordinates to determine the direction of movement.
+            - The conversion from meters to degrees is based on approximate geographical conversion factors.
+        """
         logging.info("📍  Changing geo location towards the nearest neighbor")
         scale_factor = min(1, self.max_movement_nearest_strategy / distance)
         # Calcular el ángulo hacia el vecino
@@ -88,6 +213,26 @@ class Mobility:
         await self.set_geo_location(new_latitude, new_longitude)
 
     async def set_geo_location(self, latitude, longitude):
+        """
+        Sets the geographical location of the entity to the specified latitude and longitude.
+
+        This coroutine updates the latitude and longitude values in the configuration. If the
+        provided coordinates are out of bounds (latitude must be between -90 and 90, and
+        longitude must be between -180 and 180), the previous location is retained.
+
+        Args:
+            latitude (float): The new latitude to set.
+            longitude (float): The new longitude to set.
+
+        Raises:
+            None: This function does not raise any exceptions but retains the previous coordinates
+                  if the new ones are invalid.
+
+        Notes:
+            - The new location is logged for tracking purposes.
+            - The coordinates are expected to be in decimal degrees format.
+        """
+
         if latitude < -90 or latitude > 90 or longitude < -180 or longitude > 180:
             # If the new location is out of bounds, we keep the old location
             latitude = self.config.participant["mobility_args"]["latitude"]
@@ -98,6 +243,27 @@ class Mobility:
         logging.info(f"📍  New geo location: {latitude}, {longitude}")
 
     async def change_geo_location(self):
+        """
+        Changes the geographical location of the entity based on the current mobility strategy.
+
+        This coroutine checks the mobility type and decides whether to move towards the nearest neighbor
+        or change the geo location randomly. It uses the communications manager to obtain the current
+        connections and their distances.
+
+        If the number of undirected connections is greater than directed connections, the method will
+        attempt to find the nearest neighbor and move towards it if the distance exceeds a certain threshold.
+        Otherwise, it will randomly change the geo location.
+
+        Args:
+            None: This function does not take any arguments.
+
+        Raises:
+            Exception: If the neighbor's location or distance cannot be found.
+
+        Notes:
+            - The method expects the mobility type to be either "topology" or "both".
+            - It logs actions taken during the execution for tracking and debugging purposes.
+        """
         if self.mobility and (self.mobility_type == "topology" or self.mobility_type == "both"):
             random.seed(time.time() + self.config.participant["device_args"]["idx"])
             latitude = float(self.config.participant["mobility_args"]["latitude"])
@@ -137,6 +303,28 @@ class Mobility:
             return
 
     async def change_connections_based_on_distance(self):
+        """
+        Changes the connections of the entity based on the distance to neighboring nodes.
+
+        This coroutine evaluates the current connections in the topology and adjusts their status to
+        either direct or undirected based on their distance from the entity. If a neighboring node is
+        within a certain distance, it is marked as a direct connection; otherwise, it is marked as
+        undirected.
+
+        Additionally, it updates the network conditions for each connection based on the distance,
+        ensuring that the current state is reflected accurately.
+
+        Args:
+            None: This function does not take any arguments.
+
+        Raises:
+            KeyError: If a connection address is not found during the process.
+            Exception: For any other errors that may occur while changing connections.
+
+        Notes:
+            - The method expects the mobility type to be either "topology" or "both".
+            - It logs the distance evaluations and changes made for tracking and debugging purposes.
+        """
         if self.mobility and (self.mobility_type == "topology" or self.mobility_type == "both"):
             try:
                 # logging.info(f"📍  Checking connections based on distance")
@@ -192,15 +380,36 @@ class Mobility:
                             reordering="0%",
                         )
                         self.current_network_conditions[addr] = conditions
-            except KeyError as e:
+            except KeyError:
                 # Except when self.cm.connections[addr] is not found (disconnected during the process)
-                logging.exception(f"📍  Connection {addr} not found: {e}")
+                logging.exception(f"📍  Connection {addr} not found")
                 return
-            except Exception as e:
-                logging.exception(f"📍  Error changing connections based on distance: {e}")
+            except Exception:
+                logging.exception("📍  Error changing connections based on distance")
                 return
 
     async def change_connections(self):
+        """
+        Changes the connections of the entity based on the specified mobility scheme.
+
+        This coroutine evaluates the current and potential connections at specified intervals (based
+        on the round frequency) and makes adjustments according to the mobility scheme in use. If
+        the mobility type is appropriate and the current round is a multiple of the round frequency,
+        it will proceed to change connections.
+
+        Args:
+            None: This function does not take any arguments.
+
+        Raises:
+            None: This function does not raise exceptions, but it logs errors related to connection counts
+            and unsupported mobility schemes.
+
+        Notes:
+            - The function currently supports a "random" mobility scheme, where it randomly selects
+            a current connection to disconnect and a potential connection to connect.
+            - If there are insufficient connections available, an error will be logged.
+            - All actions and decisions made by the function are logged for tracking purposes.
+        """
         if (
             self.mobility
             and (self.mobility_type == "topology" or self.mobility_type == "both")
@@ -217,8 +426,8 @@ class Mobility:
                 return
 
             if self.scheme_mobility == "random":
-                random_neighbor = random.choice(current_connections)
-                random_potential_neighbor = random.choice(potential_connections)
+                random_neighbor = random.choice(current_connections)  # noqa: S311
+                random_potential_neighbor = random.choice(potential_connections)  # noqa: S311
                 logging.info(f"📍  Selected node(s) to disconnect: {random_neighbor}")
                 logging.info(f"📍  Selected node(s) to connect: {random_potential_neighbor}")
                 await self.cm.disconnect(random_neighbor, mutual_disconnection=True)
