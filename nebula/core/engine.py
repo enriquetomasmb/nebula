dfe44c3bfef5a1fa59e6a98b0d365a19b8faf89b
Pedro Miguel Sanchez <pedromiguel.sanchez@um.es>
2024-11-15 12:02:43 +0000

    Fix issues

--- nebula/core/engine.py
+++ nebula/core/engine.py
@@ -659,7 +659,7 @@ class Engine:
         """
         nss_features = {}
         nss_features["cpu_percent"] = psutil.cpu_percent()
-        net_io_counters = psutil.net_io_counters() // 1000000
+        net_io_counters = psutil.net_io_counters()
         nss_features["bytes_sent"] = net_io_counters.bytes_sent // 1000000
         nss_features["bytes_received"] = net_io_counters.bytes_recv // 1000000
         nss_features["loss"] = self.trainer.model.loss
